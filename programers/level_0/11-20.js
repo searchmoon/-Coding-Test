45d0243471fd683d3ec164d13cc3e8ce71e2fe59
Rumi <moonding901212@gmail.com>
2023-04-27 22:19:44 +0900

    lv-0. 20번까지 완료

--- programers/level_0/11-20.js
+++ programers/level_0/11-20.js
@@ -137,3 +137,35 @@ function solution(dot) {
   }
   return answer;
 }
+
+//19. 다음에 올 숫자
+// 문제 설명
+// 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+
+function solution(common) {
+  let result = 0;
+  const difference = common[1] - common[0] === common[2] - common[1];
+  if(difference){
+    result = common[common.length-1] + (common[2] - common[1]);
+  } else {
+    result = common[common.length-1] * (common[2]/common[1]);
+  }
+  return result;
+};
+
+//20. 배열 원소의 길이
+// 문제 설명
+// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+function solution(strlist) {
+  let answer = [];
+  for(let i = 0; i < strlist.length; i++){
+    answer.push(strlist[i].length);
+  }
+  return answer;
+}
+
+// map() 을 이용한 풀이
+// function solution(strlist) {
+//     return strlist.map((el) => el.length)
+// }
