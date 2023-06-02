9a892d8df642b1fd95796cd961a4171547724e48
Rumi <moonding901212@gmail.com>
2023-06-02 18:15:08 +0900

    lv-0. 43번 완료

--- programers/level_0/41-50.js
+++ programers/level_0/41-50.js
@@ -14,3 +14,33 @@ function solution(price) {
     return price
   }
 }
+
+// 42.문자열 곱하기
+// 문제 설명
+// 문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+function solution(my_string, k) {
+  let answer = '';
+  for(let i = 0; i < k; i++){
+    answer = answer.concat(my_string);
+  }
+  return answer;
+}
+//repeat 를 이용한 풀이
+// function solution(my_string, k) {
+//   return my_string.repeat(k);
+// }
+
+// 43. 잘라서 배열로 저장하기
+// 문제 설명
+// 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(my_str, n) {
+  const strArr = my_str.split('');
+  let answer = [];
+  while(strArr.length > 0){
+    answer.push(strArr.splice(0, n).join(''));
+  }
+  return answer;
+}
+
