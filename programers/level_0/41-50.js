d07bf7e481f961b7359a686c87fea20b508ada83
Rumi <moonding901212@gmail.com>
2023-06-09 23:44:45 +0900

    lv-0. 47번까지 완료

--- programers/level_0/41-50.js
+++ programers/level_0/41-50.js
@@ -81,3 +81,13 @@ function solution(hp) {
 // }
 // ~~ 는 Math.floor() 랑 같은역할. ~~ 가 조금 더 빠르다. 고 했는데 이 풀이에서는 Math.floor 가 더 빠르네.
 
+// 47. 가위 바위 보
+// 문제 설명
+// 가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+
+function solution(rsp) {
+  let answer = '';
+  const rspObj = { 2: 0, 0: 5, 5: 2};
+  answer = [...rsp].map(x => rspObj[x]).join('');
+  return answer;
+}
