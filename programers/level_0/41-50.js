174ba177eb92dbbf69c6c0ccd8d23272eb4d0e46
Rumi <moonding901212@gmail.com>
2023-06-13 23:52:09 +0900

    lv-0. 51번까지 완료

--- programers/level_0/41-50.js
+++ programers/level_0/41-50.js
@@ -114,3 +114,22 @@ function solution(s1, s2) {
   let same = s1.filter(item => s2.includes(item));
   return same.length;
 }
+
+// 50.문자열안에 문자열
+// 문제 설명
+// 문자열 str1, str2가 매개변수로 주어집니다. str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+function solution(str1, str2) {
+  if(str1.includes(str2)){
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
+//삼항연산자를 이용한 풀이
+
+// function solution(str1, str2) {
+//     return str1.includes(str2) ? 1 : 2;
+// }
+
---
+++ programers/level_0/51-60.js
@@ -0,0 +1,19 @@
+
+//51.인덱스 바꾸기
+// 문제 설명
+// 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(my_string, num1, num2) {
+  let answer = [...my_string];
+  answer.splice(num1, 1, my_string[num2])
+  answer.splice(num2, 1, my_string[num1]);
+  return answer.join('');
+}
+
+//구조분해할당을 이용한 풀이
+
+// function solution(my_string, num1, num2) {
+//     my_string = my_string.split('');
+//     [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+//     return my_string.join('');
+// }
