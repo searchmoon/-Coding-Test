02dfac7ea7c75d8516577bf0d4b60faa075ea181
Rumi <moonding901212@gmail.com>
2023-07-23 12:10:52 +0900

    level 0 풀이

--- programers/level_0/61-70.js
+++ programers/level_0/61-70.js
@@ -30,3 +30,44 @@ function solution(my_string, is_prefix) {
 // function solution(my_string, is_prefix) {
 //   return my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0
 // }
+
+
+//64. 부분 문자열
+// 문제 설명
+// 어떤 문자열 A가 다른 문자열 B안에 속하면 A를 B의 부분 문자열이라고 합니다. 예를 들어 문자열 "abc"는 문자열 "aabcc"의 부분 문자열입니다.
+// 문자열 str1과 str2가 주어질 때, str1이 str2의 부분 문자열이라면 1을 부분 문자열이 아니라면 0을 return하도록 solution 함수를 완성해주세요.
+
+function solution(str1, str2) {
+  if(str2.includes(str1)){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+// 삼항연산자 사용한 풀이
+// function solution(str1, str2) {
+//   return str2.includes(str1) ? 1 : 0;
+// }
+
+//65. 정수 부분
+// 문제 설명
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+function solution(flo) {
+  return Math.floor(flo);
+}
+
+//66. 카운트 업
+// 문제 설명
+// 정수 start와 end가 주어질 때, start부터 end까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+function solution(start, end) {
+  let answer = [];
+  let length = end - start;
+  for(let i = start; i <= end; i++){
+    answer.push(i);
+  }
+  return answer;
+}
+
