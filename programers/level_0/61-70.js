9b8841626f86c387b61b67cbb7a4349d2d80632c
searchmoon <moonding901212@gmail.com>
2023-07-24 23:28:02 +0900

    level 0 풀이

--- programers/level_0/61-70.js
+++ programers/level_0/61-70.js
@@ -71,3 +71,58 @@ function solution(start, end) {
   return answer;
 }
 
+//67. 암호 해독
+// 문제 설명
+// 군 전략가 머쓱이는 전쟁 중 적군이 다음과 같은 암호 체계를 사용한다는 것을 알아냈습니다.
+//
+// 암호화된 문자열 cipher를 주고받습니다.
+// 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+// 문자열 cipher와 정수 code가 매개변수로 주어질 때 해독된 암호 문자열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(cipher, code) {
+  let answer = '';
+  for(let i = code - 1; i < cipher.length; i += code){
+    answer += cipher[i];
+  }
+  return answer;
+}
+
+//filter를 사용한 풀이
+//function solution(cipher, code) {
+// return cipher.split('').filter((v,i)=>(i+1)%code===0).join('');
+// }
+
+//68. 피자 나눠 먹기 (2)
+// 문제 설명
+// 머쓱이네 피자가게는 피자를 여섯 조각으로 잘라 줍니다. 피자를 나눠먹을 사람의 수 n이 매개변수로 주어질 때, n명이 주문한 피자를 남기지 않고 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+function solution(n) {
+  let pizza = 1;
+  while (pizza * 6 % n) {
+    pizza++;
+  }
+  return pizza;
+}
+
+//69.약수 구하기
+// 문제 설명
+// 정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+  let answer = [];
+  for(let i = 1; i <= n; i++){
+    if(n % i === 0){
+      answer.push(i);
+    }
+  }
+  return answer;
+}
+
+//70. 369게임
+// 문제 설명
+// 머쓱이는 친구들과 369게임을 하고 있습니다. 369게임은 1부터 숫자를 하나씩 대며 3, 6, 9가 들어가는 숫자는 숫자 대신 3, 6, 9의 개수만큼 박수를 치는 게임입니다. 머쓱이가 말해야하는 숫자 order가 매개변수로 주어질 때, 머쓱이가 쳐야할 박수 횟수를 return 하도록 solution 함수를 완성해보세요.
+
+
+function solution(order) {
+  return [...String(order)].filter((v) => ["3", "6", "9"].includes(v)).length;
+}
