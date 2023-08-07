0d5f0717bf25ce32a7d9a4ea335a3df24e5c742a
Rumi <moonding901212@gmail.com>
2023-08-07 18:49:16 +0900

    warm up 문제 풀이

--- hacker-rank/prepare/1-5.js
+++ hacker-rank/prepare/1-5.js
@@ -123,3 +123,111 @@ function simpleArraySum(ar) {
   const answer = ar.reduce((acc, crr) => acc + crr);
   return answer;
 }
+
+//3. Diagonal Difference
+
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+//
+// For example, the square matrix  is shown below:
+//
+// 1 2 3
+// 4 5 6
+// 9 8 9
+// The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+//
+// Function description
+//
+// Complete the  function in the editor below.
+//
+// diagonalDifference takes the following parameter:
+//
+// int arr[n][m]: an array of integers
+// Return
+//
+// int: the absolute diagonal difference
+// Input Format
+//
+// The first line contains a single integer, , the number of rows and columns in the square matrix .
+// Each of the next  lines describes a row, , and consists of  space-separated integers .
+//
+// Constraints
+//
+// Output Format
+//
+// Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+function diagonalDifference(arr) {
+  const length = arr.length;
+
+  let leftToRight = 0;
+  let rightToLeft = 0;
+
+  for(let i = 0; i < length; i++) {
+    leftToRight += arr[i][i];
+    rightToLeft += arr[i][(length-1)-i]
+  }
+
+  return Math.abs(leftToRight-rightToLeft);
+}
+
+//4. Plus Minus
+
+//Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+//
+// Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+//
+// Example
+//
+// There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
+//
+// 0.400000
+// 0.400000
+// 0.200000
+// Function Description
+//
+// Complete the plusMinus function in the editor below.
+//
+// plusMinus has the following parameter(s):
+//
+// int arr[n]: an array of integers
+// Print
+// Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+//
+// Input Format
+//
+// The first line contains an integer, , the size of the array.
+// The second line contains  space-separated integers that describe .
+//
+// Constraints
+//
+//
+//
+// Output Format
+//
+// Print the following  lines, each to  decimals:
+//
+// proportion of positive values
+// proportion of negative values
+// proportion of zeros
+
+function plusMinus(arr): void {
+  // Write your code here
+  let plus = 0;
+  let minus = 0;
+  let zero = 0;
+  let length = arr.length;
+
+  for(let value of arr){
+    if(value > 0) plus ++;
+    else if(value < 0) minus ++;
+    else if(value === 0) zero ++;
+  }
+
+  const result = (num) => {
+    console.log(Number(num/length).toFixed(6))
+  }
+
+  result(plus);
+  result(minus);
+  result(zero);
+}
