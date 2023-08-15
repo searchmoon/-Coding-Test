874be37725f9d225ea64b7849dc3d1920e8d35fa
searchmoon <moonding901212@gmail.com>
2023-08-15 17:38:08 +0900

    문제 풀이

--- programers/level_1/31-40.js
+++ programers/level_1/31-40.js
@@ -170,3 +170,51 @@ function solution(price, money, count) {
 
   return needMoney > money ? needMoney - money : 0;
 }
+
+
+//40.약수의 개수와 덧셈
+// 문제 설명
+// 두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 제곱근이 정수이면, 약수의 개수는 홀수인것을 참고한 풀이과정. 이게 훨씬 빠르다.
+
+function solution(left, right) {
+    let answer = 0;
+    for(let i = left; i <= right; i++){
+        if(Math.sqrt(i) % 1 === 0){
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
+
+// 이건 처음에 푼것
+// function divisorCount(num) {
+//   let count = 1;
+//
+//   for(let i = 1; i <= num/2; i++){
+//     if(num % i === 0){
+//       count++;
+//     }
+//   }
+//
+//   return count;
+// }
+//
+// function solution(left, right) {
+//   let answer = 0;
+//
+//   for(let i = left; i <= right; i++){
+//     let count = divisorCount(i) % 2;
+//     if(count === 0){
+//       answer += i;
+//     } else {
+//       answer -= i;
+//     }
+//   }
+//
+//   return answer;
+// }
