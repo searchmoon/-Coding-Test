06077c30771b488a33f59407750da08533cbfe29
searchmoon <moonding901212@gmail.com>
2023-08-19 07:48:32 +0900

    문제 풀이

--- programers/level_1/41-50.js
+++ programers/level_1/41-50.js
@@ -30,4 +30,19 @@ function solution(n) {
     return answer;
 }
 
+//43.수박수박수박수박수박수?
+// 문제 설명
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+function solution(n) {
+    let answer = [];
+    let watermelon = '수박';
+
+    for(let i = 0; i < n; i++){
+        answer.push(watermelon[i % 2]);
+    }
+
+    return answer.join('');
+}
+
 
