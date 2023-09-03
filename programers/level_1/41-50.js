910886c74ece259ed126125df4b53376a0f6e33e
Rumi <moonding901212@gmail.com>
2023-09-03 23:53:46 +0900

    문제 풀이

--- programers/level_1/41-50.js
+++ programers/level_1/41-50.js
@@ -3,18 +3,18 @@
 // 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 
 function solution(arr1, arr2) {
-    let answer = [];
+  let answer = [];
 
-    for(let i = 0; i < arr1.length; i++){
-        let sum = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let sum = [];
 
-        for(let j = 0; j < arr1[i].length; j++){
-            sum.push(arr1[i][j] + arr2[i][j]);
-        }
-
-        answer.push(sum);
+    for (let j = 0; j < arr1[i].length; j++) {
+      sum.push(arr1[i][j] + arr2[i][j]);
     }
-    return answer;
+
+    answer.push(sum);
+  }
+  return answer;
 }
 
 //42. 3진법 뒤집기
@@ -22,12 +22,12 @@ function solution(arr1, arr2) {
 // 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
 
 function solution(n) {
-    let answer = 0;
-    let reverse = n.toString(3).split('').reverse().join('');
+  let answer = 0;
+  let reverse = n.toString(3).split("").reverse().join("");
 
-    answer = parseInt(reverse, 3);
+  answer = parseInt(reverse, 3);
 
-    return answer;
+  return answer;
 }
 
 //43.수박수박수박수박수박수?
@@ -35,14 +35,14 @@ function solution(n) {
 // 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
 
 function solution(n) {
-    let answer = [];
-    let watermelon = '수박';
+  let answer = [];
+  let watermelon = "수박";
 
-    for(let i = 0; i < n; i++){
-        answer.push(watermelon[i % 2]);
-    }
+  for (let i = 0; i < n; i++) {
+    answer.push(watermelon[i % 2]);
+  }
 
-    return answer.join('');
+  return answer.join("");
 }
 
 //44.크기가 작은 부분문자열
@@ -51,16 +51,16 @@ function solution(n) {
 // 예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다. 이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
 
 function solution(t, p) {
-    let answer = 0;
-    let pLength = p.length;
+  let answer = 0;
+  let pLength = p.length;
 
-    for(let i = 0; i <= t.length - p.length; i++){
-        if(Number(t.slice(i, i + pLength)) <= p){
-            answer ++;
-        }
+  for (let i = 0; i <= t.length - p.length; i++) {
+    if (Number(t.slice(i, i + pLength)) <= p) {
+      answer++;
     }
+  }
 
-    return answer;
+  return answer;
 }
 
 //45.예산
@@ -70,21 +70,42 @@ function solution(t, p) {
 // 부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때, 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
 
 function solution(d, budget) {
-    let sum = 0;
-    let answer = 0;
-    d.sort((a,b) => a - b);
+  let sum = 0;
+  let answer = 0;
+  d.sort((a, b) => a - b);
 
-    for(let i = 0; i < d.length; i++){
-        answer++;
-        sum += d[i];
+  for (let i = 0; i < d.length; i++) {
+    answer++;
+    sum += d[i];
 
-        if(sum > budget){
-            answer--;
-        };
+    if (sum > budget) {
+      answer--;
     }
-    return answer;
+  }
+  return answer;
 }
 
-//46.
+//46.K번째수
+// 문제 설명
+// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
 
+// 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
 
+// array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+// 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+// 2에서 나온 배열의 3번째 숫자는 5입니다.
+// 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+function solution(array, commands) {
+  let answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let sortArr = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    console.log(sortArr);
+    answer.push(sortArr[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
