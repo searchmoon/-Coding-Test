3bf4fd915946877792cc8d9de0cdb06f01bdfc14
Rumi <moonding901212@gmail.com>
2023-09-05 23:37:32 +0900

    문제 풀이

--- programers/level_1/41-50.js
+++ programers/level_1/41-50.js
@@ -162,10 +162,10 @@ function solution(numbers) {
 // 입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
 
 function solution(a, b) {
-    let weeks = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    let date = new Date(`2016-${a}-${b}`);
-    let day = date.getDay();
-    return weeks[day];
+  let weeks = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let date = new Date(`2016-${a}-${b}`);
+  let day = date.getDay();
+  return weeks[day];
 }
 
 //50.문자열 내 마음대로 정렬하기
@@ -173,14 +173,14 @@ function solution(a, b) {
 // 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
 
 function solution(strings, n) {
-  strings.sort(function(a,b){
+  strings.sort(function (a, b) {
     let first = a[n];
     let second = b[n];
-    if(first === second){
+    if (first === second) {
       return (a > b) - (a < b);
-    }else{
+    } else {
       return (first > second) - (first < second);
     }
-  })
+  });
   return strings;
 }
