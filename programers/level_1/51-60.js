8c65682b704d2c72850ea1ba137554781565a53d
Rumi <moonding901212@gmail.com>
2023-09-11 23:53:04 +0900

    문제 풀이

--- programers/level_1/51-60.js
+++ programers/level_1/51-60.js
@@ -1,4 +1,4 @@
-//52. 숫자 문자열과 영단어
+//51. 숫자 문자열과 영단어
 // 문제 설명
 // 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
 // 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
@@ -18,4 +18,21 @@ function solution(s) {
 
   return Number(answer);
 }
+
+//52. 직사각형 별찍기
+// 문제 설명
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+
+  const star = "*".repeat(a);
+
+  for(let i = 0; i < b; i++){
+    console.log(star);
+  };
+});
 
