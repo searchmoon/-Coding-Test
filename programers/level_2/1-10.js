35fd63d949246b09ac27cbf639b3ebeed411e996
Rumi <moonding901212@gmail.com>
2023-09-18 01:25:48 +0900

    문제 풀이

--- programers/level_2/1-10.js
+++ programers/level_2/1-10.js
@@ -43,4 +43,32 @@ function solution(n) {
 }
 // n의 약수이면서 홀수인경우를 더하면 된다.
 
+//3. 올바른 괄호
+// 문제 설명
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+// "()()" 또는 "(())()" 는 올바른 괄호입니다.
+// ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+// '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+function solution(s) {
+  let answer = true;
+  let num = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[0] === ")") return false;
+
+    if (s[i] === "(") {
+      num++;
+    } else if (s[i] === ")") {
+      num--;
+    }
+
+    answer = num === 0 ? true : false;
+  }
+
+  return answer;
+}
+
+
 
