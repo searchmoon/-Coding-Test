3c877cc228972c4f2657e7e788510ffce9df75a2
Rumi <moonding901212@gmail.com>
2023-09-18 23:22:21 +0900

    문제 풀이

--- programers/level_2/1-10.js
+++ programers/level_2/1-10.js
@@ -32,13 +32,13 @@ function solution(s) {
 
 function solution(n) {
   let answer = 0;
-  
+
   for (let i = 0; i <= n; i++) {
     if (n % i === 0 && i % 2 === 1) {
       answer++;
     }
   }
-  
+
   return answer;
 }
 // n의 약수이면서 홀수인경우를 더하면 된다.
@@ -51,24 +51,53 @@ function solution(n) {
 // ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
 // '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
 
-function solution(s) {
-  let answer = true;
-  let num = 0;
+function solution(s){
+  let stackCount = 0;
 
   for (let i = 0; i < s.length; i++) {
-    if (s[0] === ")") return false;
+    stackCount += s[i] === '(' ? 1 : -1;
 
-    if (s[i] === "(") {
-      num++;
-    } else if (s[i] === ")") {
-      num--;
-    }
+    if (stackCount < 0) return false;
+  }
 
-    answer = num === 0 ? true : false;
+  return stackCount === 0 ? true : false;
+}
+
+//4. 이진 변환 반복하기
+// 문제 설명
+// 0과 1로 이루어진 어떤 문자열 x에 대한 이진 변환을 다음과 같이 정의합니다.
+//
+// x의 모든 0을 제거합니다.
+// x의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
+// 예를 들어, x = "0111010"이라면, x에 이진 변환을 가하면 x = "0111010" -> "1111" -> "100" 이 됩니다.
+//
+// 0과 1로 이루어진 문자열 s가 매개변수로 주어집니다. s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때, 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+
+function solution(s) {
+  let answer = [0, 0];
+  let sLen = 0;
+
+  while (s.length > 1) {
+    sLen = s.length;
+    s = s.split("0").join("");
+    answer[0]++;
+    answer[1] += (sLen - s.length);
+    s = s.length.toString(2);
   }
 
   return answer;
 }
 
+// 정규표현식을 이용한 풀이.
+// (s.match(/0/g)||[]) 에 ||[] 가 있는 이유: 0이 포함되어있나 확인하는 s.match를 했을 때, null.length 가 되지 않게 해준다.
+// function solution(s) {
+//     var answer = [0,0];
+//     while(s.length > 1) {
+//         answer[0]++;
+//         answer[1] += (s.match(/0/g)||[]).length;
+//         s = s.replace(/0/g, '').length.toString(2);
+//     }
+//     return answer;
+// }
 
 
