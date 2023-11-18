c7b9c0ec099af227c85675f05fe46a91cc7dceb1
Rumi <moonding901212@gmail.com>
2023-11-18 23:56:11 +0900

    문제풀이

--- programers/level_0/91-100.js
+++ programers/level_0/91-100.js
@@ -158,3 +158,16 @@ function solution(n, numlist) {
 // function solution(n, numlist) {
 //   return numlist.filter((num) => num % n === 0);
 //}
+
+//100. 문자열 바꿔서 찾기
+// 문제 설명
+// 문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다. myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요.
+
+function solution(myString, pat) {
+  return [...myString]
+    .map((v) => (v === "A" ? "B" : "A"))
+    .join("")
+    .includes(pat)
+    ? 1
+    : 0;
+}
